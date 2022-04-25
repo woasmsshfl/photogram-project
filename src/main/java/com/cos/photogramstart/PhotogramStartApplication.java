554aea9d6dc2a406412e90c7d059dd310023a484
stuuuu <woasmsshfl@naver.com>
2022-04-25 11:07:58 +0900

    maven build 세팅

--- src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
+++ src/main/java/com/cos/photogramstart/PhotogramStartApplication.java
@@ -3,11 +3,12 @@ package com.cos.photogramstart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PhotogramStartApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogramStartApplication.class, args);
-	}
 
+	}
 }
